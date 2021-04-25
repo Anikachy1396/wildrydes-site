2d601c94b974361aa92d077be464bb5c0b901402
Anikachy1396 <79736543+Anikachy1396@users.noreply.github.com>
2021-04-24 19:18:46 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_96nbz8pNg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7cahu5mb0v8n26fsi7v4fctv50', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
